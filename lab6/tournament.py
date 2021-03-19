cf127080715460e92fa22792e0d64f1f596b229b
Tammy Ho <tammyhho98@gmail.com>
2021-03-19 23:17:00 +0000

    Finished lab

--- lab6/tournament.py
+++ lab6/tournament.py
@@ -4,7 +4,7 @@ import csv
 import sys
 import random
 
-# Number of simluations to run
+# Number of simulations to run
 N = 1000
 
 
@@ -14,11 +14,27 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
-    teams = []
-    # TODO: Read teams into memory from file
-
+    teams = [] 
+    # Read teams into memory from file
+    with open(sys.argv[1], "r") as file:
+        # Start reading the file
+        reader = csv.DictReader(file)
+        for row in reader: 
+            # Add dictionary to teams list
+            team = {
+                'team': row['team'],
+                'rating': int(row['rating'])
+            }
+            teams.append(team)
+    
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
+    # Simulate N tournaments and keep track of win counts
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -49,7 +65,14 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
+    remaining = simulate_round(teams) 
+    while len(remaining) > 1:
+        remaining = simulate_round(remaining)
+        
+    # Access remaining team name
+    winner = remaining[0]['team']
+    
+    return winner
 
 
 if __name__ == "__main__":
